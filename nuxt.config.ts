9cda8d83a5ff41dddb8cdf8fd00dc09680f091e1
Paulo Paiva <paulo.h.paiva.c@gmail.com>
2024-01-25 20:50:44 -0300

    feat/Criação de projeto

---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  
+    routes: [
+      { name: 'home', path: '/', component: 'pages/homeApp.vue' },
+      { name: 'createAnnotation', path: '/createAnnotation', component: 'pages/createAnnotation.vue' },
+      { name: 'listApp', path: '/listApp', component: 'pages/listApp.vue' },
+    ],
+  },
+}) as any;
